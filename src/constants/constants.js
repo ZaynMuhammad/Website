b5a9920e2f3bca2c17fad826d4dbf3ac11d0f743
zaynmuhammad <zaynmuhammad4@hotmail.com>
2023-06-21 20:44:57 -0400

    Update description of chat app

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -32,7 +32,7 @@ export const projects = [
   {
     title: "Chat App",
     description:
-      "Join the same room in two seperate tabs to chat with others. This project uses Typescript, ReactJS, CSS and Socket.IO. The goal of the project was to try out Socket.IO and practice using some Typescript with React. To try out this app, make sure to join the same room on two seperate tabs or on seperate computers.",
+      "Join the same room in two seperate tabs to chat with others. The goal of the project was to try out Socket.IO and practice using some Typescript with React. To try out this app, make sure to join the same room on two seperate tabs or on seperate computers.",
     image: "/images/4.png",
     tags: ["Typescript", "CSS", "ReactJS", "Socket.IO"],
     source: "https://main--chat-app-zayn.netlify.app/",
